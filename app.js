0eb113c5772387742bda4c1bc1c493684e938c2b
Ivan Trujillo <krival1987@gmail.com>
2016-07-23 22:50:22 -0400

    first git

---
+++ app.js
@@ -0,0 +1,62 @@
+//starting backend naming our middleware and tools
+var express = require("express");
+var mongodb = require("mongodb");
+var cors = require("cors");
+var bp = require("body-parser");
+var unirest = require('unirest');
+var md5 = require("md5");
+var nodemon = require("nodemon");
+var request = require("request");
+var ObjectID = mongodb.ObjectID;
+//UNOGS KEY
+var UNOG_SKEY = process.env.UNOGS_KEY;
+
+//naming the collection to store the database
+var NETFLIX_N_CHILL_COLLECTION = "netflix";
+//setting app variable to express
+var app = express();
+//nominating the express tools to use such as body-parser and cors
+//body parser translates our data cors will allow cross origin sync
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+app.use(cors());
+//connecting mongo server to database, port 27017 also adding error handling which
+//starts after url line
+var url = 'mongodb://localhost:27017/netflix_project';
+// port that we will use mongo
+mongodb.MongoClient.connect(process.env.MONGODB_URL || url, function(err, database){
+    //error handling starts here
+    if(err){
+        console.log(err);
+        process.exit(1)
+    }
+    //naming our database
+    db = database;
+    console.log('DATABASE IS PUMPING');
+    //Starting server with express
+    var server = app.listen(process.env.PORT || 3000, function(){
+        var port = server.address().port;
+        console.log("RUNNING THE APP ON PORT", port)
+    });
+});
+//starting backend api calls and front end also error handling
+function handleError(res, reason, message, code){
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error ": message});
+}
+//HARD PART
+//We need to GET searches from API to save them
+//We need to create new events to our database
+
+
+app.post('/netflix/list', function(req, res) {//for this to work REQ is before RES
+    var inputValue = req.body.input;
+    unirest.get("https://unogs-unogs-v1.p.mashape.com/api.cgi?q=" + inputValue + "-!1900,2017-!0,5-!0,10-!0-!Any-!Any-!Any-!gt100&t=ns&cl=all&st=adv&ob=Relevance&p=1&sa=and")
+        .header("X-Mashape-Key", UNOG_SKEY)
+        .header("Accept", "application/json")
+        .end(function (result) {
+            console.log(result.status, result.headers, result.body);
+            res.send(result.body);
+            console.log(res.send)
+        })
+});
---
+++ public/main.js
@@ -0,0 +1,113 @@
+console.log('hey boo');
+    var resArr = [];
+    var movObj = {};
+    var submit = document.getElementById('submit').addEventListener('click', function(ev){
+    var input = document.getElementById('input');
+        inputValue = input.value;
+        var url = 'http://localhost:3000';
+        var data = {
+            input: inputValue
+        };
+        console.log(data);
+        $.ajax({
+            url: url + '/netflix/list',
+            method: 'POST',
+            data: data,
+            dataType: 'JSON'
+        }).done(function(response) {
+
+            movieObj(response);
+            movData(resArr)
+        });
+
+    });
+function movieObj(response) {
+    data = response.ITEMS;
+    for (var i = 0; i < data.length; i++) {
+        //console.log(data[i]);
+        //***** data[i][2]******gives poster images
+        movObj['title'] = data[i][1];
+        movObj['poster'] = data[i][2];
+        movObj['type'] = data[i][6];
+        movObj['runtime'] = data[i][8];
+        movObj['released'] = data[i][7];
+        movObj['plot'] = data[i][3];
+        resArr.push(movObj);
+        movObj = {}
+    }
+    //console.log(resArr);
+    return resArr
+}
+//function to push append information to screen each div will have it's own id, and it will be incremented by i as per the loop
+     function movData(resArr){
+         for (var i = 0; i < resArr.length; i++){
+             info = document.getElementById('info');
+             divs = document.createElement('div');
+             divs.classList.add('stats');
+             ul = document.createElement('ul');
+             favorites = document.createElement('button');
+             favorites.classList.add('buttons');
+             deletes = document.createElement('button');
+             deletes.classList.add('buttons');
+             fButton = document.createTextNode('Add to Chill');
+             dButton = document.createTextNode('No Chill');
+             imgDiv = document.createElement('div');
+             imgDiv.classList.add('image');
+             favorites.appendChild(fButton);
+             deletes.appendChild(dButton);
+             info.appendChild(divs);
+             divs.appendChild(ul);
+             divs.appendChild(favorites);
+             divs.appendChild(deletes);
+             info.appendChild(imgDiv);
+             for (var movie in resArr[i]){
+                 if (movie ==='title'){
+                     li = document.createElement('li');
+                     header = document.createElement('h2');
+                     titles = document.createTextNode(resArr[i].title);
+                     header.appendChild(titles);
+                     li.appendChild(titles);
+                     ul.appendChild(li)
+                 }
+             }
+             for (var movie in resArr[i]){
+                 if (movie === 'type'){
+                     li = document.createElement('li');
+                     type = document.createTextNode(resArr[i].type);
+                     li.appendChild(type);
+                     ul.appendChild(li)
+                 }
+             }
+             for (var movie in resArr[i]){
+                 if(movie === 'runtime'){
+                     li = document.createElement('li');
+                     runtime = document.createTextNode(resArr[i].runtime);
+                     li.appendChild(runtime);
+                     ul.appendChild(li)
+                 }
+             }
+             for (var movie in resArr[i]){
+                 if(movie === 'released'){
+                     li = document.createElement('li');
+                     released = document.createTextNode(resArr[i].released);
+                     li.appendChild(released);
+                     ul.appendChild(li)
+                 }
+             }
+             for (var movie in resArr[i]){
+                 if(movie === 'plot'){
+                     li = document.createElement('li');
+                     plot = document.createTextNode(resArr[i].plot);
+                     li.appendChild(plot);
+                     ul.appendChild(li)
+                 }
+             }
+             for(var movie in resArr[i]);{
+                 if(movie === 'poster'){
+                     imgDiv.setAttribute('src', resArr[i].poster)
+                 }
+             }
+         }
+     }
+
+
